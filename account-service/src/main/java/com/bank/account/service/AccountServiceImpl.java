50ae9e91d670a0ce11c0d72ef9ec31e2998eb4e1
Ankit Kumar <an20696793@wipro.com>
2026-06-02 08:59:53 +0530

    circuit breaker

--- account-service/src/main/java/com/bank/account/service/AccountServiceImpl.java
+++ account-service/src/main/java/com/bank/account/service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.bank.account.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +20,9 @@ public class AccountServiceImpl implements AccountService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
     @Override
     public Account addAccount(Account account) {
@@ -38,6 +42,14 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public AccountCustomerVO getAccountWithCustomer(Long accountId) {
 
+        return circuitBreakerFactory.create("customerService").run(
+                () -> getAccountWithCustomerFromCustomerService(accountId),
+                throwable -> customerServiceFallback(accountId, throwable)
+        );
+    }
+
+    private AccountCustomerVO getAccountWithCustomerFromCustomerService(Long accountId) {
+
         Account account = accountRepository.findById(accountId).orElse(null);
 
         if (account == null) {
@@ -52,6 +64,28 @@ public class AccountServiceImpl implements AccountService {
         AccountCustomerVO accountCustomerVO = new AccountCustomerVO();
         accountCustomerVO.setAccount(account);
         accountCustomerVO.setCustomer(customer);
+
+        return accountCustomerVO;
+    }
+
+    public AccountCustomerVO customerServiceFallback(Long accountId, Throwable throwable) {
+
+        Account account = accountRepository.findById(accountId).orElse(null);
+
+        if (account == null) {
+            return null;
+        }
+
+        Customer fallbackCustomer = new Customer();
+        fallbackCustomer.setCustomerId(0L);
+        fallbackCustomer.setCustomerName("Customer Service Unavailable");
+        fallbackCustomer.setEmail("N/A");
+        fallbackCustomer.setPhone("N/A");
+        fallbackCustomer.setAddress("Fallback Response");
+
+        AccountCustomerVO accountCustomerVO = new AccountCustomerVO();
+        accountCustomerVO.setAccount(account);
+        accountCustomerVO.setCustomer(fallbackCustomer);
 
         return accountCustomerVO;
     }
